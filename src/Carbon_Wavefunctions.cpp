818833de6ed92601903ca82f87e1a0de26e42877
Timon Emken <timon.emken@fysik.su.se>
2022-10-26 14:52:18 +0200

    New Carbon_Wavefunction child class: Roothaan Hartree Fock wavefunctions including unit tests

--- src/Carbon_Wavefunctions.cpp
+++ src/Carbon_Wavefunctions.cpp
@@ -1,9 +1,16 @@
 #include "graphene/Carbon_Wavefunctions.hpp"
 
 #include <cmath>
+#include <fstream>
+
+#include "acb_hypgeom.h"
+
+#include <boost/math/special_functions/factorials.hpp>
 
 #include "libphysica/Integration.hpp"
 #include "libphysica/Natural_Units.hpp"
+
+#include "version.hpp"
 
 namespace graphene
 {
@@ -97,6 +104,147 @@ std::complex<double> Hydrogenic::Wavefunction_Momentum(const Eigen::Vector3d& kV
 	{
 		std::cerr << "Error in Hydrogenic::Wavefunction_Position(): Unknown orbital " << orbital << std::endl;
 		exit(EXIT_FAILURE);
+	}
+}
+
+// 3. Roothaan-Hartree-Fock wavefunctions
+double a0 = Bohr_Radius;
+void Roothaan_Hartree_Fock::Import_RHF_Coefficients()
+{
+	std::vector<std::string> orbitals = {"2s", "2p"};
+	for(int i = 0; i < 2; i++)
+	{
+		std::string filepath = TOP_LEVEL_DIR "data/C_" + orbitals[i] + ".txt";
+		std::ifstream f;
+		f.open(filepath);
+		if(f.is_open())
+		{
+			double binding_energy;
+			f >> binding_energy;
+			double C, Z;
+			unsigned int nin;
+			while(f >> nin >> Z >> C)
+			{
+				n_lj[i].push_back(nin);
+				Z_lj[i].push_back(Z);
+				C_nlj[i].push_back(C);
+			}
+			f.close();
+		}
+	}
+}
+
+double Roothaan_Hartree_Fock::Radial_Wavefunction_Position(double r, const std::string& orbital) const
+{
+	int i		= orbital == "2s" ? 0 : 1;
+	double R_nl = 0.0;
+	for(unsigned int j = 0; j < C_nlj[i].size(); j++)
+		R_nl += C_nlj[i][j] * std::pow(2.0 * Z_lj[i][j], n_lj[i][j] + 0.5) / sqrt(boost::math::factorial<double>(2.0 * n_lj[i][j])) * std::pow(r / a0, n_lj[i][j] - 1.0) * std::exp(-Z_lj[i][j] * r / a0);
+
+	return std::pow(a0, -1.5) * R_nl;
+}
+
+std::complex<double> Hypergeometric_2F1(double a, double b, double c, double z)
+{
+	using namespace std::complex_literals;
+	std::complex<double> result;
+	slong prec;
+	acb_t F_arb, a_arb, b_arb, c_arb, z_arb;
+	acb_init(F_arb);
+	acb_init(a_arb);
+	acb_init(b_arb);
+	acb_init(c_arb);
+	acb_init(z_arb);
+	acb_set_d(a_arb, a);
+	acb_set_d(b_arb, b);
+	acb_set_d(c_arb, c);
+	acb_set_d(z_arb, z);
+	for(prec = 80;; prec *= 2)
+	{
+		acb_hypgeom_2f1(F_arb, a_arb, b_arb, c_arb, z_arb, false, prec);
+		if(acb_rel_accuracy_bits(F_arb) >= 53)
+		{
+			arb_t F_real, F_imag;
+			arb_init(F_real);
+			arb_init(F_imag);
+			acb_get_real(F_real, F_arb);
+			acb_get_imag(F_imag, F_arb);
+			result = arf_get_d(arb_midref(F_real), ARF_RND_NEAR) + 1i * arf_get_d(arb_midref(F_imag), ARF_RND_NEAR);
+			break;
+		}
+		else if(prec > 10000)
+		{
+			result = NAN;
+			break;
+		}
+	}
+	acb_clear(F_arb);
+	acb_clear(a_arb);
+	acb_clear(b_arb);
+	acb_clear(c_arb);
+	acb_clear(z_arb);
+	return result;
+}
+
+std::complex<double> Roothaan_Hartree_Fock::Radial_Wavefunction_Momentum(double p, const std::string& orbital) const
+{
+	using namespace std::complex_literals;
+	int i					  = orbital == "2s" ? 0 : 1;
+	int l					  = orbital == "2s" ? 0 : 1;
+	std::complex<double> R_nl = 0.0;
+	for(unsigned int j = 0; j < C_nlj[i].size(); j++)
+	{
+		// Hypergeometric function 2F1(a,b,c,z)
+		double a								= 0.5 * (2.0 + l + n_lj[i][j]);
+		double b								= 0.5 * (3.0 + l + n_lj[i][j]);
+		double c								= 1.5 + l;
+		double z								= -std::pow(p * Bohr_Radius / Z_lj[i][j], 2.0);
+		std::complex<double> hypergeometric_2F1 = Hypergeometric_2F1(a, b, c, z);
+
+		R_nl += std::pow(2.0 * M_PI * Bohr_Radius / Z_lj[i][j], 1.5) * C_nlj[i][j] * std::pow(2.0, -l + n_lj[i][j]) * boost::math::factorial<double>(1.0 + n_lj[i][j] + l) / sqrt(boost::math::factorial<double>(2.0 * n_lj[i][j])) * std::pow(1i * p * Bohr_Radius / Z_lj[i][j], l) * hypergeometric_2F1 / tgamma(1.5 + l);
+	}
+	return R_nl;
+}
+
+Roothaan_Hartree_Fock::Roothaan_Hartree_Fock()
+: C_nlj({{}, {}}), Z_lj({{}, {}}), n_lj({{}, {}})
+{
+	Import_RHF_Coefficients();
+}
+
+double Roothaan_Hartree_Fock::Wavefunction_Position(const Eigen::Vector3d& xVec, const std::string& orbital)
+{
+	double r = xVec.norm();
+	if(orbital == "2s")
+		return 1.0 / sqrt(4.0 * M_PI) * Radial_Wavefunction_Position(r, orbital);
+	else if(orbital == "2px")
+		return sqrt(3.0 / 4.0 / M_PI) * xVec[0] / r * Radial_Wavefunction_Position(r, orbital);
+	else if(orbital == "2py")
+		return sqrt(3.0 / 4.0 / M_PI) * xVec[1] / r * Radial_Wavefunction_Position(r, orbital);
+	else if(orbital == "2pz")
+		return sqrt(3.0 / 4.0 / M_PI) * xVec[2] / r * Radial_Wavefunction_Position(r, orbital);
+	else
+	{
+		std::cout << "Error in Roothaan_Hartree_Fock::Wavefunction_Position(): Orbital " << orbital << " not recognized." << std::endl;
+		std::exit(EXIT_FAILURE);
+	}
+}
+
+std::complex<double> Roothaan_Hartree_Fock::Wavefunction_Momentum(const Eigen::Vector3d& kVec, const std::string& orbital)
+{
+	double k = kVec.norm();
+	if(orbital == "2s")
+		return 1.0 / sqrt(4.0 * M_PI) * Radial_Wavefunction_Momentum(kVec.norm(), orbital);
+	else if(orbital == "2px")
+		return sqrt(3.0 / 4.0 / M_PI) * kVec[0] / k * Radial_Wavefunction_Momentum(k, orbital);
+	else if(orbital == "2py")
+		return sqrt(3.0 / 4.0 / M_PI) * kVec[1] / k * Radial_Wavefunction_Momentum(k, orbital);
+	else if(orbital == "2pz")
+		return sqrt(3.0 / 4.0 / M_PI) * kVec[2] / k * Radial_Wavefunction_Momentum(k, orbital);
+	else
+	{
+		std::cout << "Error in Roothaan_Hartree_Fock::Wavefunction_Momentum(): Orbital " << orbital << " not recognized." << std::endl;
+		std::exit(EXIT_FAILURE);
 	}
 }
 
--- tests/test_Carbon_Wavefunctions.cpp
+++ tests/test_Carbon_Wavefunctions.cpp
@@ -12,6 +12,7 @@
 using namespace libphysica::natural_units;
 using namespace graphene;
 
+// 2. Hydrogenic wavefunctions
 TEST(TestHydrogenicWavefunctions, TestPositionWavefunctionValues)
 {
 	// ARRANGE
@@ -67,4 +68,58 @@ TEST(TestHydrogenicWavefunctions, TestMomentumWavefunctionNormalization)
 	// ACT & ASSERT
 	for(auto orbital : orbitals)
 		EXPECT_NEAR(hydrogenic.Normalization_Momentum(orbital), 1.0, tol);
+}
+
+// 3. Roothaan-Hartree-Fock wavefunctions
+TEST(TestRoothaanHartreeFock, TestPositionWavefunctionValues)
+{
+	Eigen::Vector3d position({Bohr_Radius, Bohr_Radius / 2, Bohr_Radius / 3});
+	Roothaan_Hartree_Fock rhf;
+	double phi_2s  = -1.414450631e-9;
+	double phi_2px = 1.877396747e-09;
+	double phi_2py = 9.386983734e-10;
+	double phi_2pz = 6.257989156e-10;
+	// ACT & ASSERT
+	EXPECT_FLOAT_EQ(rhf.Wavefunction_Position(position, "2s"), phi_2s);
+	EXPECT_FLOAT_EQ(rhf.Wavefunction_Position(position, "2px"), phi_2px);
+	EXPECT_FLOAT_EQ(rhf.Wavefunction_Position(position, "2py"), phi_2py);
+	EXPECT_FLOAT_EQ(rhf.Wavefunction_Position(position, "2pz"), phi_2pz);
+}
+
+TEST(TestRoothaanHartreeFock, TestPositionWavefunctionNormalization)
+{
+	// ARRANGE
+	double tol = 1e-4;
+	Roothaan_Hartree_Fock rhf;
+	std::vector<std::string> orbitals = {"2s", "2px", "2py", "2pz"};
+	// ACT & ASSERT
+	for(auto orbital : orbitals)
+		EXPECT_NEAR(rhf.Normalization_Position(orbital), 1.0, tol);
+}
+
+TEST(TestRoothaanHartreeFock, TestMomentumWavefunctionValues)
+{
+	// ARRANGE
+	Eigen::Vector3d momentum({1.0 / Bohr_Radius, 2 / Bohr_Radius, 3 / Bohr_Radius});
+	Roothaan_Hartree_Fock rhf;
+	double phi_2s  = 2.354591573e7;
+	double phi_2px = 1.0460452717e7;
+	double phi_2py = 2.092090543e7;
+	double phi_2pz = 3.138135815e7;
+	// ACT & ASSERT
+	EXPECT_FLOAT_EQ(std::abs(rhf.Wavefunction_Momentum(momentum, "2s")), phi_2s);
+	EXPECT_FLOAT_EQ(std::abs(rhf.Wavefunction_Momentum(momentum, "2px")), phi_2px);
+	EXPECT_FLOAT_EQ(std::abs(rhf.Wavefunction_Momentum(momentum, "2py")), phi_2py);
+	EXPECT_FLOAT_EQ(std::abs(rhf.Wavefunction_Momentum(momentum, "2pz")), phi_2pz);
+}
+
+TEST(TestRoothaanHartreeFock, TestMomentumWavefunctionNormalization)
+{
+	// ARRANGE
+	double tol = 1e-4;
+	Roothaan_Hartree_Fock rhf;
+	std::vector<std::string> orbitals = {"2s", "2px", "2py", "2pz"};
+	// ACT & ASSERT
+	for(auto orbital : orbitals)
+		EXPECT_NEAR(rhf.Normalization_Momentum(orbital), 1.0, tol);
 }
