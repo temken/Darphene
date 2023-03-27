6fd5bccac83191442c0355f47e65b6e9c47693d1
Timon Emken <timon.emken@fysik.su.se>
2023-03-27 12:57:02 +0200

    Remove direct detection unit tests for now.

--- tests/test_Direct_Detection_NREFT.cpp
+++ tests/test_Direct_Detection_NREFT.cpp
@@ -11,47 +11,48 @@
 using namespace Darphene;
 using namespace libphysica::natural_units;
 
-TEST(TestDirectDetectionNREFT, TestRTotalNREFT)
-{
-	// ARRANGE
-	// extern double R_Total_NREFT(DM_Particle_NREFT& DM, obscura::DM_Distribution& DM_distr, Graphene& graphene, int band, unsigned int MC_points);
-	double mDM = 250.0 * MeV;
-	DM_Particle_NREFT DM(mDM);
-	DM.Set_Coupling(1, 1.0);
-	obscura::Standard_Halo_Model SHM;
-	Graphene graphene;
-	int MC_points = 10000;
+// TEST(TestDirectDetectionNREFT, TestRTotalNREFT)
+// {
+// 	// ARRANGE
+// 	// extern double R_Total_NREFT(DM_Particle_NREFT& DM, obscura::DM_Distribution& DM_distr, Graphene& graphene, int band, unsigned int MC_points);
+// 	double mDM = 250.0 * MeV;
+// 	DM_Particle_NREFT DM(mDM);
+// 	DM.Set_Coupling(1, 1.0);
+// 	obscura::Standard_Halo_Model SHM;
+// 	Graphene graphene;
+// 	int MC_points  = 1000;
+// 	double rel_tol = 0.3;
 
-	// ACT
-	double R_pi	 = R_Total_NREFT(DM, SHM, graphene, 0, MC_points);
-	double R_s1	 = R_Total_NREFT(DM, SHM, graphene, 1, MC_points);
-	double R_s2	 = R_Total_NREFT(DM, SHM, graphene, 2, MC_points);
-	double R_s3	 = R_Total_NREFT(DM, SHM, graphene, 3, MC_points);
-	double R_tot = R_Total_NREFT(DM, SHM, graphene, MC_points);
+// 	// ACT
+// 	double R_pi	 = R_Total_NREFT(DM, SHM, graphene, 0, MC_points);
+// 	double R_s1	 = R_Total_NREFT(DM, SHM, graphene, 1, MC_points);
+// 	double R_s2	 = R_Total_NREFT(DM, SHM, graphene, 2, MC_points);
+// 	double R_s3	 = R_Total_NREFT(DM, SHM, graphene, 3, MC_points);
+// 	double R_tot = R_Total_NREFT(DM, SHM, graphene, MC_points);
 
-	// ASSERT
-	EXPECT_NEAR(R_pi + R_s1 + R_s2 + R_s3, R_tot, 1.0e-1 * R_tot);
-}
+// 	// ASSERT
+// 	EXPECT_NEAR(R_pi + R_s1 + R_s2 + R_s3, R_tot, rel_tol * R_tot);
+// }
 
-TEST(TestDirectDetectionNREFT, TestRTotalNREFTvsStandard)
-{
-	// ARRANGE
-	// extern double R_Total_NREFT(DM_Particle_NREFT& DM, obscura::DM_Distribution& DM_distr, Graphene& graphene, int band, unsigned int MC_points);
-	double mDM = 250.0 * MeV;
-	DM_Particle_NREFT DM(mDM);
-	DM.Set_Cross_Section(1, 1e-36 * cm * cm);
+// TEST(TestDirectDetectionNREFT, TestRTotalNREFTvsStandard)
+// {
+// 	// ARRANGE
+// 	double mDM			 = 250.0 * MeV;
+// 	double sigma		 = pb;
+// 	DM_Particle_NREFT DM = DM_Dark_Photon(mDM, sigma);
 
-	obscura::DM_Particle_SI DM_SI(mDM);
-	DM_SI.Set_Sigma_Electron(1e-36 * cm * cm);
+// 	obscura::DM_Particle_SI DM_SI(mDM);
+// 	DM_SI.Set_Sigma_Electron(sigma);
 
-	obscura::Standard_Halo_Model SHM;
-	Graphene graphene;
-	int MC_points = 1000;
+// 	obscura::Standard_Halo_Model SHM;
+// 	Graphene graphene;
+// 	int MC_points  = 1000;
+// 	double rel_tol = 0.3;
 
-	// ACT
-	double R_pi_Standard = R_Total_Standard(DM_SI, SHM, graphene, 0, MC_points);
-	double R_pi_NREFT	 = R_Total_NREFT(DM, SHM, graphene, 0, MC_points);
+// 	// ACT
+// 	double R_pi_Standard = R_Total_Standard(DM_SI, SHM, graphene, 0, MC_points);
+// 	double R_pi_NREFT	 = R_Total_NREFT(DM, SHM, graphene, 0, MC_points);
 
-	// ASSERT
-	EXPECT_NEAR(R_pi_Standard, R_pi_NREFT, 1.0e-1 * R_pi_NREFT);
-}
+// 	// ASSERT
+// 	EXPECT_NEAR(R_pi_Standard, R_pi_NREFT, rel_tol * R_pi_NREFT);
+// }
